1ce05f13db7d373989fe6ce9d052a5afc04f84aa
Dylan Dover <dylan.b.dover@vanderbilt.edu>
2016-04-14 16:06:23 -0500

    idk

--- Server/GhostGrabServer.js
+++ Server/GhostGrabServer.js
@@ -157,24 +157,19 @@ dispatcher.onPost("/move", function(req, res) {
 
 // update leaderboard
 dispatcher.onPost("/updateleaderboard", function(req, res) {
-
     var topTenNamesAndScores = [];
 
-<<<<<<< Updated upstream
     for(var key in myMap){
+    	console.log("test");
+    	console.log(key + '/n');
+    	console.log(myMap.get(key));
         topTenNamesAndScores.push([key, myMap.get(key)]);
         topTenNamesAndScores.sort(function(a, b){
             return (b[1] - a[1]);
-=======
-    for(var key, value of myMap){
-        topTenNamesAndScores.push({key, value});
-        topTenNamesAndScores.sort(function({k1,v1}, {k2,v2}){
-            return (v2 - v1);
->>>>>>> Stashed changes
         });
     }
+    console.log(topTenNamesAndScores.slice(0,9));
     res.end(JSON.stringify(topTenNamesAndScores.slice(0,9)));
-    res.end('Got Post Data');
 });
 
 // create a server
