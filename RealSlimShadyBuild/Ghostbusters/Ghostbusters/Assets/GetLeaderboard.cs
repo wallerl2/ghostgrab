448b9918c3e1cca4fe3a9711de0755245f2daac8
Dylan Dover <dylan.b.dover@vanderbilt.edu>
2016-04-14 22:56:48 -0500

    Some stuff works so that's nice.

--- RealSlimShadyBuild/Ghostbusters/Ghostbusters/Assets/GetLeaderboard.cs
+++ RealSlimShadyBuild/Ghostbusters/Ghostbusters/Assets/GetLeaderboard.cs
@@ -8,7 +8,7 @@ public class GetLeaderboard : MonoBehaviour {
     // Use this for initialization
     void Start() {
         Hashtable data = new Hashtable();
-        String temp;
+		string[] splitResponse = { };
 
         data.Add("name", NameManager.userName);
         data.Add("score:", NameManager.score);
@@ -16,21 +16,41 @@ public class GetLeaderboard : MonoBehaviour {
         HTTP.Request someRequest2 = new HTTP.Request("post", "http://ghostgrab.ddns.net:5001/updateleaderboard", data);
         someRequest2.Send((request) => {
             // parse some JSON, for example:
-            String str = request.response.Text;
-            UnityEngine.Debug.Log("POST");
-            UnityEngine.Debug.Log(str);
-            
-        });
+            String response = request.response.Text;
 
-        int counter = 1;
-        foreach(Transform t in transform)
-        {
-            Text[] texts = t.GetComponentsInChildren<Text>();
-            texts[0].text = "#" + counter;
-            texts[1].text = "USERzzz";
-            texts[2].text = "Score:" + "wtf";
-            counter++;
-        }
+            UnityEngine.Debug.Log("POST");
+            UnityEngine.Debug.Log(response);
+
+			// remove all brackets
+			response = response.Replace("[", "");
+			response = response.Replace("]", "");
+			UnityEngine.Debug.Log(response);
+
+			// split on commas
+			char[] comma = {','};
+			splitResponse = response.Split(comma);
+
+			// update leaderboard
+			int editsToMake = splitResponse.Length;
+			int counter = 0;
+			int location = 0;
+			foreach(Transform t in transform) {
+				Text[] texts = t.GetComponentsInChildren<Text>();
+
+				if (counter < editsToMake) {
+					texts[0].text = "#" + counter;
+					texts[1].text = splitResponse[location];
+					texts[2].text = "Score: " + splitResponse [location + 1];
+				} else {
+					texts[0].text = "#" + counter;
+					texts[1].text = "No User";
+					texts[2].text = "Score: N/A";
+				}
+				counter += 1;
+				location += 2;
+			}
+        });
+       
     }
 	
 	// Update is called once per frame
